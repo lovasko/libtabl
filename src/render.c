1443cfed183222e5234f4f2f151307edf9723002
Daniel Lovasko <lovasko@freebsd.org>
2015-06-19 05:46:33 +0100

    various features in the render code

--- src/render.c
+++ src/render.c
@@ -1,55 +1,56 @@
+#include <stdio.h>
+
 #include "tabl.h"
+#include "column.h"
 
 static void
-render_column(void* _col, void* fd)
+render_column(void* _col, void* payload)
+{
+	struct column* col;
+	
+	(void)payload;
+	col = _col;
+	printf(col->align == TABL_ALIGN_LEFT ? "%-*s " : "%*s ",
+	       col->max_width,
+	       col->name);
+}
+
+static void
+render_value(void* _col, void* value, void* payload)
 {
 	struct column* col;
 
 	(void)payload;
 	col = _col;
-
-	dprintf(*((int*)fd), "%*s ", col->max_width, col->name);
-}
-
-static void
-render_value(void* _col, void* value, void* fd)
-{
-	struct column* col;
-
-	col = _col;
 	switch (col->content) {
 		case TABL_CONTENT_STRING:
-			dprintf(*((int*)fd), "%*s ", col->max_width, (char*)value);
+			printf(col->align == TABL_ALIGN_LEFT ? "%-*s " : "%*s ",
+			       col->max_width,
+			       (char*)value);
 		return;
 
 		case TABL_CONTENT_DECIMAL:
-			dprintf(*((int*)fd), "%*d ", col->max_width, *((int*)value));
+			printf("%*d ", col->max_width, *((int*)value));
 		return;
 	}
 }
 
 static void
-render_row(void* row, void* arg)
+render_row(void* row, void* t)
 {
-	struct render_arg* rarg;
-
-	rarg = arg;
-	m_list_zip(&arg->t->columns, row, render_value, arg);
+	m_list_zip(&((struct tabl*)t)->columns, row, render_value, NULL);
+	printf("\n");
 }
 
 int
-tabl_render(struct tabl* t, int fd)
+tabl_render(struct tabl* t)
 {
-	struct render_arg rarg;
-
 	if (t == NULL)
 		return TABL_E_NULL;
 
-	rarg.t = t;
-	rarg.fd = fd;
-
-	m_list_map(&t->columns, print_column, &fd);
-	m_list_map(&t->rows, render_row, &rarg);
+	m_list_map(&t->columns, render_column, NULL);
+	printf("\n");
+	m_list_map(&t->rows, render_row, t);
 
 	return TABL_OK;
 }
