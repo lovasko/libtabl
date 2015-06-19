98de55ce550e761ae65d0355a9208a528d6143df
Daniel Lovasko <lovasko@freebsd.org>
2015-06-19 05:42:27 +0100

    various small fixed in the add code

--- src/add.c
+++ src/add.c
@@ -1,4 +1,5 @@
 #include <math.h>
+#include <string.h>
 
 #include "tabl.h"
 #include "column.h"
@@ -14,7 +15,7 @@ tabl_add_column(struct tabl* t, const char* name, uint8_t content, uint8_t align
 
 	m_list_size(&t->rows, &row_count);
 	if (row_count > 0)
-		return TABL_E_ROWS_EXIST;
+		return TABL_E_ROWS;
 
 	col.name = strdup(name);
 	col.content = content;
@@ -44,12 +45,12 @@ extend_width(void* _col, void* value, void* payload)
 			if (*((int*)value) == 0)
 				width = 1;
 			else 
-				width = (unsigned int)floor(log10(fabs(*((int*)n))))+1;
+				width = (unsigned int)floor(log10(fabs(*((int*)value))))+1;
 		break;
 	}
 	
 	if (width > col->max_width)
-		col->max_width = width;	
+		col->max_width = width;
 }
 
 int
@@ -57,6 +58,7 @@ tabl_add_row(struct tabl* t, struct m_list* values)
 {
 	uint64_t column_count;
 	uint64_t values_count;
+	struct m_list* row;
 
 	m_list_size(&t->columns, &column_count);
 	m_list_size(values, &values_count);
@@ -66,9 +68,10 @@ tabl_add_row(struct tabl* t, struct m_list* values)
 
 	m_list_zip(&t->columns, values, extend_width, NULL);
 
-	row = malloc(sizeof(m_list));
+	row = malloc(sizeof(struct m_list));
+	m_list_init(row);
 	m_list_copy(values, row, M_LIST_COPY_DEEP);
-	m_list_append(&t->rows, row, M_LIST_COPY_SHALLOW, 0);
+	m_list_append(&t->rows, M_LIST_COPY_SHALLOW, row, 0);
 		
 	return TABL_OK;
 }
